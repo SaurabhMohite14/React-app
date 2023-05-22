8ed1907ce46433fcd0ae5ef3c26183b3ca1a0cff
SaurabhMohite14 <saurabhmohite2214@gmail.com>
2023-05-22 10:44:07 +0530

    multi use of user tag

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-return (<div>
-  <h1>Hello World</h1>
-</div>);
+  return (
+    <div>
+    <Header></Header>
+      <h1>Hello World</h1>
+      <Header />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div>
+      <h1>Saurabh Mohite</h1>
+      
+    </div>
+  );
 }
 
 export default App;
