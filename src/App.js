3b81650d5a55a93543629b1340a13ee14fbb13b2
SaurabhMohite14 <saurabhmohite2214@gmail.com>
2023-05-22 10:33:07 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
-//Note: This is known as JSX=> JAVASCRIPT AND XML
-//creating a behind tag ***(user defined tag)**** <App/> 
-//<App /> :: <h1> <p> <div>
-//USer Defined Tag: <App><App/>  or <App/>
-//X Stand For::Extended Markup Language
-//XML:: User Defined TAG:
-//XML :: There must be one root tag,Parent.
+//Code Formating
+//Auto save
+//Auto Suggestion
+//Run the program :: npm start
 
 function App() {
-return <div> 
-  <h1>Hello World</h1>
-  <p>Saurabh BRo</p>
-</div>/**Parent tag div */
+retu
 }
 
 export default App;
