ee59be8fa5c0c08ffdcf8688fc8effd3600c8b02
SaurabhMohite14 <saurabhmohite2214@gmail.com>
2023-05-22 10:00:37 +0530

    Hello_project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+//Note:File NAme. IT starts with Capayial letterr
+//Note: File name and the function name are same.
+//Note: Make the module public 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return "Hello World";
 }
 
 export default App;
