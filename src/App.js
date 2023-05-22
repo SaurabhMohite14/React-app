1fd267c743d0d288e52fa7d3f657279d35e2219b
SaurabhMohite14 <saurabhmohite2214@gmail.com>
2023-05-22 10:57:28 +0530

    outside-function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import saurabh from "./pages/Header";
 function App() {
   return (
     <div>
-    <Header></Header>
-    <MainBody/>
+      <Header></Header>
+      <MainBody />
       <h1>Hello World</h1>
       <Header />
-      <Footer/>
+      <Footer />
+      <saurabh />
     </div>
   );
 }
@@ -17,19 +18,23 @@ function Header() {
   return (
     <div>
       <h1>Saurabh Mohite</h1>
-      
     </div>
   );
 }
 
-function MainBody(){
-  return <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia temporibus magnam, eveniet animi molestias ipsam quae obcaecati qui. Natus nulla dolorem porro eaque doloremque soluta aliquam iure itaque, eligendi facilis!</div>
+function MainBody() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia temporibus
+      magnam, eveniet animi molestias ipsam quae obcaecati qui. Natus nulla
+      dolorem porro eaque doloremque soluta aliquam iure itaque, eligendi
+      facilis!
+    </div>
+  );
 }
 
-function Footer(){
-  return <div>
-This is footer
-  </div>
+function Footer() {
+  return <div>This is footer</div>;
 }
 
 export default App;
---
+++ src/pages/Header.js
@@ -0,0 +1,8 @@
+function saurabh() {
+  return (
+    <div>
+      <h1>this Is header</h1>
+    </div>
+  );
+}
+export default saurabh;
