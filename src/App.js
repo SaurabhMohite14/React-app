70937dd70c6fcd9f6c099ddcafeece99cba1ef1e
SaurabhMohite14 <saurabhmohite2214@gmail.com>
2023-05-22 10:20:29 +0530

    Multiple Tag

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import './App.css';
 //creating a behind tag ***(user defined tag)**** <App/> 
 //<App /> :: <h1> <p> <div>
 //USer Defined Tag: <App><App/>  or <App/>
+//X Stand For::Extended Markup Language
+//XML:: User Defined TAG:
+//XML :: There must be one root tag,Parent.
 
 function App() {
-return <h1>Hello World</h1>
-
+return <div> 
+  <h1>Hello World</h1>
+  <p>Saurabh BRo</p>
+</div>/**Parent tag div */
 }
 
 export default App;
