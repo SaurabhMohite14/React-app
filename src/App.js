df95bec130a084d69eb87fff5c0167f937251da0
SaurabhMohite14 <saurabhmohite2214@gmail.com>
2023-05-22 11:41:13 +0530

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,37 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Registration from "./Rgistration";
-import Head from "./pages/Header";
+
 function App() {
-  return (
-    <div>
-      <Head />
-      <Registration />
-    </div>
-  );
+  return <div></div>;
 }
-
-/**function Header() {
-  return (
-    <div>
-      <h1>Saurabh Mohite</h1>
-    </div>
-  );
-}
-
-function MainBody() {
-  return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia temporibus
-      magnam, eveniet animi molestias ipsam quae obcaecati qui. Natus nulla
-      dolorem porro eaque doloremque soluta aliquam iure itaque, eligendi
-      facilis!
-    </div>
-  );
-}
-
-function Footer() {
-  return <div>This is footer</div>;
-}*/
-
 export default App;
